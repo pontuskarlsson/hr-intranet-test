0381aba5ffca0ee82bf7e19f8df6c80f75514d2e
Daniel Wiklund <daniel@happyrabbit.com>
2014-09-10 18:21:56 +0800

    Added Employee Focus to home page

--- app/models/refinery/user.rb
+++ app/models/refinery/user.rb
@@ -42,6 +42,18 @@ module Refinery
         value = conditions[authentication_keys.first]
         where(["username = :value OR email = :value", { :value => value }]).first
       end
+
+      def random(number_of_records = 1)
+        # Determines if it is necessary to randomize. For example, if
+        # there are only two records and number_of_records is 2, then
+        # just return those.
+        max_offset = count - ( number_of_records - 1 )
+        if max_offset <= 1
+          scoped
+        else
+          offset(rand(max_offset)).limit(number_of_records)
+        end
+      end
     end
 
     def plugins=(plugin_names)
