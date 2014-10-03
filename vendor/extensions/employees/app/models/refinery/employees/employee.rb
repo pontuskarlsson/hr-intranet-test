fda11d06e0e17d05ac68e82d62132772e5a3250a
Daniel Wiklund <daniel@happyrabbit.com>
2014-10-03 18:17:15 +0800

    Fixed problem with assigning user to employee

--- vendor/extensions/employees/app/models/refinery/employees/employee.rb
+++ vendor/extensions/employees/app/models/refinery/employees/employee.rb
@@ -12,12 +12,25 @@ module Refinery
       has_many :xero_expense_claims,  dependent: :destroy
       has_many :xero_receipts,        dependent: :destroy
 
-      attr_accessible :user_id, :employee_no, :full_name, :id_no, :profile_image_id, :title, :position, :xero_guid
+      attr_writer :user_name
+      attr_accessible :user_id, :employee_no, :full_name, :id_no, :profile_image_id, :title, :position, :xero_guid, :user_name
 
       validates :employee_no, presence: true, uniqueness: true
       validates :full_name,   presence: true
       validates :user_id,     uniqueness: true, allow_nil: true
       validates :xero_guid,   uniqueness: true, allow_blank: true
+
+      before_validation do
+        if @user_name.present?
+          if (user = ::Refinery::User.find_by_full_name(@user_name)).present?
+            self.user = user
+          end
+        end
+      end
+
+      def user_name
+        @user_name ||= user.try(:full_name)
+      end
 
       def current_employment_contract
         @_cec ||= employment_contracts.where(end_date: nil).first
