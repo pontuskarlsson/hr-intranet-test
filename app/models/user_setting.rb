7b7c41e68b000cb3118ddb14b2b114cc072ca7e8
Daniel Wiklund <daniel@happyrabbit.com>
2015-07-23 09:27:47 +0800

    Fixed issue with adding new User

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -5,7 +5,7 @@ class UserSetting < ActiveRecord::Base
 
   attr_accessible :content, :identifier
 
-  validates :user_id,     presence: true
+  validates :user_id,     presence: true, unless: :new_record?
   validates :identifier,  presence: true, uniqueness: { scope: :user_id }
 
 end
