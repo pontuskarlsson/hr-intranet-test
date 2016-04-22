0ec7834e940c00b4f08a8e1588ba243dc97eb7db
Daniel Wiklund <daniel@happyrabbit.com>
2016-04-22 14:02:14 +0800

    Improved address handling for Shipments

--- vendor/extensions/parcels/app/helpers/refinery/parcels/shipments_helper.rb
+++ vendor/extensions/parcels/app/helpers/refinery/parcels/shipments_helper.rb
@@ -3,8 +3,9 @@ module Refinery
     module ShipmentsHelper
 
       def contact_references
-        @_contact_references ||= ::Refinery::Marketing::Contact.select([:id, :name, :organisation_name, :address]).map { |contact|
-          { value: contact.id, label: contact.name, organisation_name: contact.organisation_name, address: contact.address }
+        @_contact_references ||= ::Refinery::Marketing::Contact.includes(:organisation).select([:id, :name, :organisation_name, :address]).map { |contact|
+          organisation_name = contact.organisation_name.blank? ? contact.organisation.try(:name) : contact.organisation_name
+          { value: contact.id, label: contact.name, organisation_name: organisation_name, address: contact.address }
         }.to_json
       end
 
