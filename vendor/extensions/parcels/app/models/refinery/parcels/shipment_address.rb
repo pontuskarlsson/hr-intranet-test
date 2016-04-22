611a44772ff50d89bc316c17c618034ce599a4ae
Daniel Wiklund <daniel@happyrabbit.com>
2016-04-22 11:58:46 +0800

    Improved address handling for Shipments

--- vendor/extensions/parcels/app/models/refinery/parcels/shipment_address.rb
+++ vendor/extensions/parcels/app/models/refinery/parcels/shipment_address.rb
@@ -9,12 +9,14 @@ module Refinery
       attr_accessible :name, :street1, :street2, :city, :zip, :state, :country, :phone, :email
 
       def assign_from_contact(contact)
+        org = contact.organisation
+
         self.name     = contact.name
-        self.street1  = contact.address
-        #self.state    = contact.state     # Contact does not have state information...
-        self.zip      = contact.zip
-        self.city     = contact.city
-        self.country  = contact.country
+        self.street1  = contact.address.present?  ? contact.address : org.try(:address)
+        self.state    = contact.state.present?    ? contact.state   : org.try(:state)
+        self.zip      = contact.zip.present?      ? contact.zip     : org.try(:zip)
+        self.city     = contact.city.present?     ? contact.city    : org.try(:city)
+        self.country  = contact.country.present?  ? contact.country : org.try(:country)
         self.phone    = [contact.phone, contact.mobile].reject(&:blank?).first
         self.email    = contact.email
         self.company  = contact.organisation_name
@@ -26,6 +28,10 @@ module Refinery
             "#{attr} is missing"
           end
         }.compact
+      end
+
+      def google_maps_url
+        "http://maps.google.com/?q=#{ [street1, city, zip, state, country].reject(:blank?).join(',') }"
       end
 
     end
