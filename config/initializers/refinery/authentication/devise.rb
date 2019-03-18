0dcdc466b47050ca03191062fd8f443d561b30ae
Daniel Wiklund <daniel@happyrabbit.com>
2019-03-18 12:25:10 +0800

    Allow superusers to assign roles

--- config/initializers/refinery/authentication/devise.rb
+++ config/initializers/refinery/authentication/devise.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 Refinery::Authentication::Devise.configure do |config|
   # Configure whether to allow superuser to assign roles
-  config.superuser_can_assign_roles = false
+  config.superuser_can_assign_roles = true
 
   # Configure notification email from name
   # config.email_from_name = "no-reply"
