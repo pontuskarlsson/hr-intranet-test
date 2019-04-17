6fa6a937b7130118cf1bce076a2f18ba81a37313
Daniel Wiklund <daniel@happyrabbit.com>
2019-04-17 14:12:36 +0800

    Fix incorrect param key name

--- vendor/extensions/refinerycms-employees/app/controllers/refinery/employees/admin/leave_of_absences_controller.rb
+++ vendor/extensions/refinerycms-employees/app/controllers/refinery/employees/admin/leave_of_absences_controller.rb
@@ -8,7 +8,7 @@ module Refinery
                 order: 'start_date DESC'
 
         def leave_of_absence_params
-          params.require(:leave_of_avsence).permit(
+          params.require(:leave_of_absence).permit(
               :start_date, :start_half_day, :end_date, :end_half_day, :doctors_note_id, :employee_name,
               :absence_type_id, :absence_type_description, :status, :i_am_sick_today
           )
