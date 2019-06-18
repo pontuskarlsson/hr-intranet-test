b91e349f4bc432f1def0cedf4f20ded53221b90d
Daniel Wiklund <daniel@happyrabbit.com>
2019-06-18 15:48:39 +0800

    Change html email attachments to inline

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,13 +1,13 @@
 module EmailHelper
 
   def dragonfly_image_tag(image, **options)
-    attachments[image.name] ||= image.thumb('190x190#').data
-    image_tag attachments[image.name].url, **options
+    attachments.inline[image.name] ||= image.thumb('190x190#').data
+    image_tag attachments.inline[image.name].url, **options
   end
 
   def asset_image_tag(image_path, **options)
-    attachments[image_path] ||= File.read(Rails.root.join("app/assets/images/#{image_path}"))
-    image_tag attachments[image_path].url, **options
+    attachments.inline[image_path] ||= File.read(Rails.root.join("app/assets/images/#{image_path}"))
+    image_tag attachments.inline[image_path].url, **options
   end
 
 end
