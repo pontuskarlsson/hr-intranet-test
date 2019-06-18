d2f84df5b8dca8591bceb308b44ea8b41bfa2381
Daniel Wiklund <daniel@happyrabbit.com>
2019-06-18 09:44:10 +0800

    Change 500 response template

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -22,7 +22,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = 'Happy Rabbit Intranet'
+  config.site_name = 'Happy Rabbit Portal'
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
