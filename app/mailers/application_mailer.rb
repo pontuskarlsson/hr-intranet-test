9aa8e03c051efd173860cb8c74fb35805b678e6d
Daniel Wiklund <daniel@happyrabbit.com>
2019-07-02 18:22:38 +0800

    Fix issue with invalid url_methods called.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper(EmailHelper)
+  include Devise::Controllers::UrlHelpers
 
   default from: "\"Happy Rabbit\" <noreply@happyrabbit.com>"
 
