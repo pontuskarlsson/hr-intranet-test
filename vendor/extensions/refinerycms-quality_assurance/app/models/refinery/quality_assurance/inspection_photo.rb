43c297131b7b1806830a701671d2411de0441f1e
Daniel Wiklund <daniel@happyrabbit.com>
2019-08-29 14:49:27 +0800

    Separate inspection photos into multiple sections

--- vendor/extensions/refinerycms-quality_assurance/app/models/refinery/quality_assurance/inspection_photo.rb
+++ vendor/extensions/refinerycms-quality_assurance/app/models/refinery/quality_assurance/inspection_photo.rb
@@ -3,6 +3,11 @@ module Refinery
     class InspectionPhoto < Refinery::Core::BaseModel
       self.table_name = 'refinery_quality_assurance_inspection_photos'
 
+      REGEX_MEASUREMENT = /\AMeasurement\.[0-9]+\.MeasurementPhoto\z|\Acc\.[0-9]+\.cd\z/
+      REGEX_PRODUCT = /\AProduct\.[0-9]+\.ProductPhoto\z|\Afp\.[0-9]+\.fq\z/
+      REGEX_PREVIEW = /\APreview\z|\Ad\z/
+      REGEX_DEFECT = /\ADefect\.[0-9]+\.DefectPhoto\.[0-9]+\.DefectPhotoCanvas\z|\Acj\.[0-9]+\.do\.[0-9]+\.dp\z/
+
       serialize :fields, Hash
 
       belongs_to :inspection
@@ -30,6 +35,26 @@ module Refinery
         if image.present?
           size.nil? ? image.image.url : image.image.thumb(size).url
         end
+      end
+
+      def measurement_photo?
+        (fields['key'] || '')[REGEX_MEASUREMENT]
+      end
+
+      def product_photo?
+        (fields['key'] || '')[REGEX_PRODUCT]
+      end
+
+      def preview_photo?
+        (fields['key'] || '')[REGEX_PREVIEW]
+      end
+
+      def defect_photo?
+        (fields['key'] || '')[REGEX_DEFECT]
+      end
+
+      def other_photo?
+        !measurement_photo? && !product_photo? && !preview_photo? && !defect_photo?
       end
 
     end
