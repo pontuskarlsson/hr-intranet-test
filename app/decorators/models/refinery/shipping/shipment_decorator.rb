533910e64b2db1c228182c9d8245aa56ba900ec6
Daniel Wiklund <daniel@happyrabbit.com>
2019-10-21 18:19:32 +0800

    Enable notificatinos for external users

--- app/decorators/models/refinery/shipping/shipment_decorator.rb
+++ app/decorators/models/refinery/shipping/shipment_decorator.rb
@@ -4,14 +4,16 @@ Refinery::Shipping::Shipment.class_eval do
   acts_as_notifiable :'refinery/authentication/devise/users',
                      targets: ->(shipment, key) {
                        internal_users = Refinery::Authentication::Devise::User.where(email: ['daniel.viklund@happyrabbit.com', 'pontus.karlsson@happyrabbit.com', 'yvonne.wong@happyrabbit.com'])
-                       # ff_users = Refinery::Authentication::Devise::User.for_role(Refinery::Shipping::ROLE_EXTERNAL_FF).for_companies(shipment.forwarder_company)
-                       # con_users = Refinery::Authentication::Devise::User.for_role(Refinery::Shipping::ROLE_EXTERNAL).for_companies(shipment.consignee_company)
-                       #
-                       # if key['shipment.status_request']
-                       #   internal_users + ff_users
-                       # elsif key['shipment.status_summary']
-                       #   internal_users + ff_users + con_users
-                       # end
+                       ff_users = Refinery::Authentication::Devise::User.for_role(Refinery::Shipping::ROLE_EXTERNAL_FF).for_companies(shipment.forwarder_company)
+                       con_users = Refinery::Authentication::Devise::User.for_role(Refinery::Shipping::ROLE_EXTERNAL).for_companies(shipment.consignee_company)
+
+                       if key['shipment.status_request']
+                         internal_users + ff_users
+                       elsif key['shipment.status_summary']
+                         internal_users + ff_users + con_users
+                       else
+                         internal_users
+                       end
                      },
                      tracked: false, # no automatic callbacks
                      # group: :article,
