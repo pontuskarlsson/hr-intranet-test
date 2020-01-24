1f374b4226db87595fbde4c239d6926f99070319
Daniel Wiklund <daniel@happyrabbit.com>
2020-01-24 13:42:17 +0800

    Implement redis as session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_portal_session', domain: :all
+#Rails.application.config.session_store :cookie_store, key: '_portal_session', domain: :all
+
+Rails.application.config.session_store :redis_store, {
+    servers: [
+        { host: ENV['REDIS_HOST'], port: 6379, db: 0 },
+    ],
+    key: '_happy_rabbit_session',
+    domain: :all
+}
