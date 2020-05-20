d4d934e3e8783422ff433892461ab3c4dbe927bb
Daniel Wiklund <daniel@happyrabbit.com>
2020-05-20 17:12:56 +0800

    Change dashboard view for internal users to look more like it does for external users

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,6 +1,8 @@
 class PortalController < ApplicationController
   before_action :find_page
 
+  helper_method :recent_requests
+
   def dashboard
     template = @page&.link_url == "/" ? "home" : "show"
     render template: "refinery/pages/#{@page&.view_template.presence || template}"
@@ -11,6 +13,14 @@ class PortalController < ApplicationController
   def find_page
     @page = ::Refinery::Page.find_by_link_url(request.original_fullpath.chomp('/'))
     not_found unless @page.present?
+  end
+
+  def requests_scope
+    ::Refinery::Business::Request.for_selected_company(selected_company).for_user_roles(current_refinery_user)
+  end
+
+  def recent_requests(limit = 5)
+    requests_scope.order(request_date: :desc).limit(limit)
   end
 
 end
